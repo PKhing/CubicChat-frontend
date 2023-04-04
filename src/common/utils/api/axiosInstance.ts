d9036aa015382520ed3148b6997aae294a129bda
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-04-04 10:10:13 +0700

    feat: add axios

---
+++ src/common/utils/api/axiosInstance.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const BASE_URL =
+  import.meta.env.VITE_ENABLE_PROXY === 'true'
+    ? '/api'
+    : import.meta.env.VITE_API_BASE_URL
+
+console.log(import.meta.env.ENABLE_PROXY, import.meta.env.VITE_API_BASE_URL)
+
+export const apiClient = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  withCredentials: true,
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
