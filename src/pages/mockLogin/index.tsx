70564c275ccabe59cef3b71bed32c9d0aeaba090
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-04-04 10:10:20 +0700

    feat: add mock login page

---
+++ src/pages/mockLogin/index.tsx
@@ -0,0 +1,33 @@
+import { apiClient } from 'common/utils/api/axiosInstance'
+import React from 'react'
+
+const MockLogin = () => {
+  console.log({ base: import.meta.env.VITE_API_BASE_URL })
+  const handleLogin1 = () => {
+    apiClient.post('/auth/login', {
+      email: 'mafumafu@gmail.com',
+      password: 'password',
+    })
+  }
+  const handleLogin2 = () => {
+    apiClient.post('/auth/login', {
+      email: 'soraru@gmail.com',
+      password: 'password',
+    })
+  }
+
+  const handleLogUser = async () => {
+    const res = await apiClient.get('/profile')
+    console.log(res.data)
+  }
+
+  return (
+    <div>
+      <button onClick={handleLogin1}>Account 1</button>
+      <button onClick={handleLogin2}>Account 2</button>
+      <button onClick={handleLogUser}>Log user</button>
+    </div>
+  )
+}
+
+export default MockLogin
--- src/route.tsx
+++ src/route.tsx
@@ -1,9 +1,14 @@
 import Home from 'pages/home'
+import MockLogin from 'pages/mockLogin'
 import React from 'react'
 
 export const route = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/mock-login',
+    element: <MockLogin />,
   },
 ]
